59ca34fcd72638d05cf9f23ae2c468173914e0a8
zohar <zohar@MacBook-Pro-de-zohar.local>
2019-05-30 16:57:25 +0300

    premier commi

--- heyguis /script.js
+++ heyguis /script.js
@@ -1,2 +1,17 @@
-function addannimal(name, country){
+animaux = [];
+
+function addanimal (name,country) {
+    animal = {
+        'animalName': name,
+        'animalCountry':country
+    }
+    animaux.push(animal)
+    console.log(animaux)
 }
+
+$(".ajout_animal").on("click", function(){
+  var name = $('.name_input').val();
+  var country = $('.country_input').val();
+  console.log(name,country)
+  addanimal(name,country)
+});
